1a967e4f308b8acde4cea71456cb7fba6620d37d
alsdnr4874 <alsdnr4874@gmail.com>
2020-06-22 15:24:15 +0900

    계산식 표현, 함수 사용하여 데이터 입력받도록 수정

--- cal_cui.py
+++ cal_cui.py
@@ -1,19 +1,58 @@
+
+def input_num():
+    temp = input("계산할 정수 입력 : ")
+
+    if  temp.isdecimal() :
+        return int(temp)
+    else :
+        return None
+
+def input_opt():
+    temp = input("기호입력 : ")
+
+    if  (temp == '+') | (temp == '-') | (temp == '*') | (temp == '/'):
+        return temp
+    else :
+        return None
+
 while True:
-    num = int(input("계산할 정수 입력 : "))
-    opt = input("기호입력 : ")
+    res, temp, num1, num2 = 0, 0, 0, 0
+    form = ''
+
+    num1 = input_num()
+    if  num1 == None :
+        print("정수만 입력할 것\n")
+        continue
     
+    form = str(num1)
+
+    opt = input_opt()
+    if  opt == None :
+        print("기호만 입력할 것\n")
+        continue
+    
+    form = form + opt
+
+    num2 = input_num()
+    if  num2 == None :
+        print("정수만 입력할 것\n")
+        continue   
+    
+    form = form + str(num2)
+
     if opt == '+':
-        num = num + int(input("계산할 정수 입력 : "))
+        res = num1 + num2
     elif opt == '-':
-        num = num - int(input("계산할 정수 입력 : "))
+        res = num1 - num2
     elif opt == '*':
-        num = num * int(input("계산할 정수 입력 : "))
+        res = num1 * num2
     elif opt == '/':
-        num = num / int(input("계산할 정수 입력 : "))
+        res = num1 / num2
     else :
         print("사칙연산만 지원합니다.")
 
-    print("계산 결과는 {}입니다.".format(num))
+    print("입력된 수식은 " + form + " 입니다.\n")
+    print("계산 결과는 {}입니다.".format(res))
 
     
 
